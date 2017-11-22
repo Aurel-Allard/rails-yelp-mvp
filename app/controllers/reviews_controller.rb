00dbab36302d5bdcc26ac923b6a4fdc60d1c7e69
Aurelien Allard <allard.au@gmail.com>
2017-11-22 16:39:48 +0100

    Yelp exercise terminé

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,8 +14,11 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     # we need `restaurant_id` to asssociate review with corresponding restaurant
     @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
-    redirect_to review_path(@review)
+    if @review.save
+      redirect_to restaurant_path(params[:restaurant_id])
+    else
+      render :new
+    end
   end
 
   private
